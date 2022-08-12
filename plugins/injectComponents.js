56ffa166b151ce915f62249ada44532802907cf6
SergeyKovalchuk2086 <kovalchuk.s.v@inbox.ru>
2022-08-12 13:06:00 +0700

    [update] create wiki page

--- plugins/injectComponents.js
+++ plugins/injectComponents.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+import Header from '~/components/app/Header';
+import Footer from '~/components/app/Footer';
 import BaseInput from '~/components/ui/BaseInput';
 import BaseButton from '~/components/ui/BaseButton';
 import CtmModal from '~/components/CtmModal';
@@ -11,3 +13,5 @@ Vue.component('base-btn', BaseButton);
 Vue.component('ctm-modal', CtmModal);
 Vue.component('ctm-modal-box', CtmModalBox);
 Vue.component('base-textarea', BaseTextarea);
+Vue.component('Header', Header);
+Vue.component('Footer', Footer);
